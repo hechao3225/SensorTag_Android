d86ce1984f2004403a8c84d5d12ed7abc5027963
hechao3225 <328237809@qq.com>
2017-05-19 14:14:31 +0800

    SensorTag  APP
    TI 官方安卓工具，用于测试CC2640的传感器控制器

---
+++ app/build/generated/source/buildConfig/debug/ti/android/ble/sensortag/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ti.android.ble.sensortag;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ti.android.ble.sensortag";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "1.10";
+}
